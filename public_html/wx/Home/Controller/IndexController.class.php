9311d560662e1ee31a7d42c15958cd5d68f825f5
lifetea <15068107015@sina.cn>
2015-11-13 16:42:10 +0800

    finxs #2 cert

--- public_html/wx/Home/Controller/IndexController.class.php
+++ public_html/wx/Home/Controller/IndexController.class.php
@@ -19,7 +19,7 @@ class IndexController extends Controller
     public function cert()
     {
 		
-		$wechatObj = new wechatCallbackapiTest();
+		$wechatObj = new Lib\WebChat();
 		if (isset($_GET['echostr'])) {
 			$wechatObj->valid();
 		}else{
