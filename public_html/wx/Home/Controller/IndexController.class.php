550a685c945b8a81cd4173fa614683de59da2354
lifetea <15068107015@sina.cn>
2015-11-13 14:40:39 +0800

    fixs #2 menu

--- public_html/wx/Home/Controller/IndexController.class.php
+++ public_html/wx/Home/Controller/IndexController.class.php
@@ -2,15 +2,34 @@
 namespace Home\Controller;
 
 use Think\Controller;
+use Home\Lib;
+use Org\Util;
 
 class IndexController extends Controller
 {
     public function index()
     {
        $this->display();
-   }
+   	}
+
    	public function test()
     {
        $this->display();
-   }
+   	}
+    public function cert()
+    {
+		
+		$wechatObj = new wechatCallbackapiTest();
+		if (isset($_GET['echostr'])) {
+			$wechatObj->valid();
+		}else{
+			$wechatObj->responseMsg();
+		}
+   	}  	
+   	public function menu(){
+   		$sdk = new Org\JSSDK();
+   		$ticket  = $sdk->getJsApiTicket();
+   		var_dump($ticket);
+   		//$wechatObj = new Lib\WeChat();
+   	}
 }
