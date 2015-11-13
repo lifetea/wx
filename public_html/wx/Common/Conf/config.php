24e6aff09e45c6c7f8ef45fdce882cb36e00c432
lifetea <15068107015@sina.cn>
2015-11-13 15:43:03 +0800

    fixs #2 menu

--- public_html/wx/Common/Conf/config.php
+++ public_html/wx/Common/Conf/config.php
@@ -1,4 +1,49 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'menuJson'=>'{
+		      "button":[
+		      {
+		           "name":"天气预报",
+		           "sub_button":[
+		            {
+		               "type":"click",
+		               "name":"北京天气",
+		               "key":"天气北京"
+		            },
+		            {
+		               "type":"click",
+		               "name":"深圳天气",
+		               "key":"天气深圳"
+		            },
+		            {
+		                "type":"view",
+		                "name":"本地天气",
+		                "url":"http://m.hao123.com/a/tianqi"
+		            }]
+		      
+
+		       },
+		       {
+		           "name":"车侣威擎",
+		           "sub_button":[
+		            {
+		               "type":"click",
+		               "name":"公司简介",
+		               "key":"company"
+		            },
+		            {
+		               "type":"click",
+		               "name":"趣味游戏",
+		               "key":"游戏"
+		            },
+		            {
+		                "type":"click",
+		                "name":"讲个笑话",
+		                "key":"笑话"
+		            }]
+		       
+
+		       }]
+		 }',
 );
--- public_html/wx/Home/Controller/IndexController.class.php
+++ public_html/wx/Home/Controller/IndexController.class.php
@@ -27,57 +27,11 @@ class IndexController extends Controller
 		}
    	}  	
    	public function menu(){
-		 $jsonmenu = '{
-		      "button":[
-		      {
-		            "name":"天气预报",
-		           "sub_button":[
-		            {
-		               "type":"click",
-		               "name":"北京天气",
-		               "key":"天气北京"
-		            },
-		            {
-		               "type":"click",
-		               "name":"深圳天气",
-		               "key":"天气深圳"
-		            },
-		            {
-		                "type":"view",
-		                "name":"本地天气",
-		                "url":"http://m.hao123.com/a/tianqi"
-		            }]
-		      
-
-		       },
-		       {
-		           "name":"车侣威擎",
-		           "sub_button":[
-		            {
-		               "type":"click",
-		               "name":"公司简介",
-		               "key":"company"
-		            },
-		            {
-		               "type":"click",
-		               "name":"趣味游戏",
-		               "key":"游戏"
-		            },
-		            {
-		                "type":"click",
-		                "name":"讲个笑话",
-		                "key":"笑话"
-		            }]
-		       
-
-		       }]
-		 }';  		
+		$jsonmenu = C("menuJson");  		
    		$sdk = new Util\JSSDK();
    		$access_token  = $sdk->getAccessToken();
-   		var_dump($access_token);
    		$url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$access_token;
 		$result = $sdk->https_request($url, $jsonmenu);
 		var_dump($result);
-   		//$wechatObj = new Lib\WeChat();
    	}
 }
