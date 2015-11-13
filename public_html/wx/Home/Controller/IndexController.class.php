a7a6fdbebf6de5b08443d02b3758a1329e1f1389
lifetea <15068107015@sina.cn>
2015-11-13 15:05:12 +0800

    fixs #2 menu

--- public_html/wx/Home/Controller/IndexController.class.php
+++ public_html/wx/Home/Controller/IndexController.class.php
@@ -27,9 +27,56 @@ class IndexController extends Controller
 		}
    	}  	
    	public function menu(){
+		 $jsonmenu = '{
+		      "button":[
+		      {
+		            "name":"天气预报",
+		           "sub_button":[
+		            {
+		               "type":"click",
+		               "name":"北京天气",
+		               "key":"天气北京"
+		            },
+		            {
+		               "type":"click",
+		               "name":"深圳天气",
+		               "key":"天气深圳"
+		            },
+		            {
+		                "type":"view",
+		                "name":"本地天气",
+		                "url":"http://m.hao123.com/a/tianqi"
+		            }]
+		      
+
+		       },
+		       {
+		           "name":"车侣威擎",
+		           "sub_button":[
+		            {
+		               "type":"click",
+		               "name":"公司简介",
+		               "key":"company"
+		            },
+		            {
+		               "type":"click",
+		               "name":"趣味游戏",
+		               "key":"游戏"
+		            },
+		            {
+		                "type":"click",
+		                "name":"讲个笑话",
+		                "key":"笑话"
+		            }]
+		       
+
+		       }]
+		 }';  		
    		$sdk = new Util\JSSDK();
-   		$ticket  = $sdk->getJsApiTicket();
-   		var_dump($ticket);
+   		$access_token  = $sdk->getJsApiTicket();
+   		$url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=".$access_token;
+		$result = $sdk->https_request($url, $jsonmenu);
+		var_dump($result);
    		//$wechatObj = new Lib\WeChat();
    	}
 }
