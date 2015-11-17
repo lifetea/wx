56cb67efa5a72ddc7a79a6b7a077d19802456e80
lifetea <15068107015@sina.cn>
2015-11-17 13:18:27 +0800

    fixs #3

--- public_html/ThinkPHP/Library/Org/Util/JSSDK.class.php
+++ public_html/ThinkPHP/Library/Org/Util/JSSDK.class.php
@@ -90,6 +90,7 @@ class JSSDK {
     if (!$token) {
       $appId = $this->appId;
       $secret = $this->appSecret;
+      var_dump($code);
       $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid={$appid}&secret={$secret}&code={$code}&grant_type=authorization_code";
       $res = file_get_contents($url);
       $res = json_decode($res, true);
