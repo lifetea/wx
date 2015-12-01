07914aa506e65c9832dbc781544ca96b5de5a8b6
lifetea <15068107015@sina.cn>
2015-12-01 15:51:23 +0800

    fixs #3 update

--- public_html/ThinkPHP/Library/Org/Util/JSSDK.class.php
+++ public_html/ThinkPHP/Library/Org/Util/JSSDK.class.php
@@ -5,8 +5,8 @@ class JSSDK {
   private $appSecret;
 
   public function __construct($appId, $appSecret) {
-    $this->appId = "wxd2e82d66cc76016c";
-    $this->appSecret = "7d2d31c0120ad6812a7403a87403825b";
+    $this->appId = "wxd2e82d66cc76016c";//wxd2e82d66cc76016c
+    $this->appSecret = "7fd7e90c834f1d29c8bae9b484a9a72d";//7d2d31c0120ad6812a7403a87403825b
   }
 
   public function getSignPackage() {
