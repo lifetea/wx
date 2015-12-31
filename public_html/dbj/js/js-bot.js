e6dee010e8808835d28d3f36b9e944c1ca160209
lifetea <15068107015@sina.cn>
2015-12-31 14:55:21 +0800

    fixs #7 大宝剑

---
+++ public_html/dbj/js/js-bot.js
@@ -0,0 +1,12 @@
+//统计代码
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?1842c91510bf015a8ff26d773fe44f3d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+//广告代码
+var cpro_id = "u1937771";
+document.write('<script src="http://cpro.baidustatic.com/cpro/ui/cm.js" type="text/javascript"></script>');
